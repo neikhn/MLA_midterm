515d3aaf0056869c52f3f683e1a9aae25dc1bd45
Chau <vichau6@gmail.com>
2024-10-19 20:19:36 +0700

    plot three random question

--- starter_code/part_a/item_response.py
+++ starter_code/part_a/item_response.py
@@ -1,6 +1,9 @@
+import matplotlib
+from matplotlib import pyplot as plt
+
 from utils import *
 
-import numpy as np
+matplotlib.use('TkAgg')
 
 
 def sigmoid(x):
@@ -12,18 +15,12 @@ def sigmoid(x):
 def neg_log_likelihood(data, theta, beta):
     """ Compute the negative log-likelihood.
 
-    You may optionally replace the function arguments to receive a matrix.
-
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
     :param theta: Vector
     :param beta: Vector
     :return: float
     """
-    #####################################################################
-    # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
-    #####################################################################
     log_lklihood = 0.
     for i, q in enumerate(data['question_id']):
         u = data['user_id'][i]
@@ -34,21 +31,11 @@ def neg_log_likelihood(data, theta, beta):
         p = sigmoid(theta_u - beta_q)
         # Accumulate the log-likelihood
         log_lklihood += c * np.log(p) + (1 - c) * np.log(1 - p)
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
     return -log_lklihood
 
 
 def update_theta_beta(data, lr, theta, beta):
     """ Update theta and beta using gradient descent.
-
-    You are using alternating gradient descent. Your update should look:
-    for i in iterations ...
-        theta <- new_theta
-        beta <- new_beta
-
-    You may optionally replace the function arguments to receive a matrix.
 
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
@@ -57,10 +44,6 @@ def update_theta_beta(data, lr, theta, beta):
     :param beta: Vector
     :return: tuple of vectors
     """
-    #####################################################################
-    # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
-    #####################################################################
     for i, q in enumerate(data['question_id']):
         u = data['user_id'][i]
         c = data['is_correct'][i]
@@ -71,16 +54,11 @@ def update_theta_beta(data, lr, theta, beta):
         # Update the gradients
         theta[u] += lr * (c - p)
         beta[q] -= lr * (c - p)
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
     return theta, beta
 
 
 def irt(data, val_data, lr, iterations):
     """ Train IRT model.
-
-    You may optionally replace the function arguments to receive a matrix.
 
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
@@ -90,7 +68,7 @@ def irt(data, val_data, lr, iterations):
     :param iterations: int
     :return: (theta, beta, val_acc_lst)
     """
-    # TODO: Initialize theta and beta.
+    # Initialize theta and beta.
     theta = np.random.randn(max(data["user_id"]) + 1)
     beta = np.random.randn(max(data["question_id"]) + 1)
 
@@ -103,7 +81,7 @@ def irt(data, val_data, lr, iterations):
         print("Iteration: {} \t NLLK: {} \t Score: {}".format(i, neg_lld, score))
         theta, beta = update_theta_beta(data, lr, theta, beta)
 
-    # TODO: You may change the return values to achieve what you want.
+    # You may change the return values to achieve what you want.
     return theta, beta, val_acc_lst
 
 
@@ -126,6 +104,31 @@ def evaluate(data, theta, beta):
            / len(data["is_correct"])
 
 
+def plot_prob_vs_theta(theta, beta, question_ids):
+    """ Plot the probability of correct response as a function of student ability (theta)
+        for the given questions.
+
+    :param theta: Vector of student abilities
+    :param beta: Vector of question difficulties
+    :param question_ids: List of question ids to plot for
+    """
+    theta_range = np.linspace(-4, 4, 100)  # Range of student abilities (θ)
+
+    plt.figure(figsize=(10, 6))
+
+    for q_id in question_ids:
+        # For each question, calculate the probability of correct response as a function of θ
+        probabilities = sigmoid(theta_range - beta[q_id])
+        plt.plot(theta_range, probabilities, label=f"Question {q_id}")
+
+    plt.title("Probability of Correct Response vs Student Ability (θ)")
+    plt.xlabel("Student Ability (θ)")
+    plt.ylabel("Probability of Correct Response")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
 def main():
     train_data = load_train_csv("data")
     # You may optionally use the sparse matrix.
@@ -133,32 +136,21 @@ def main():
     val_data = load_valid_csv("data")
     test_data = load_public_test_csv("data")
 
-    #####################################################################
-    # TODO:                                                             #
-    # Tune learning rate and number of iterations. With the implemented #
-    # code, report the validation and test accuracy.                    #
-    #####################################################################
     # Tune learning rate and number of iterations
     lr = 0.01  # Learning rate
     iterations = 100  # Number of iterations
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
-
-    #####################################################################
-    # TODO:                                                             #
-    # Implement part (d)                                                #
-    #####################################################################
     # Train the model
     theta, beta, val_acc_lst = irt(train_data, val_data, lr, iterations)
     # Evaluate on the test data
     test_acc = evaluate(test_data, theta, beta)
     print("Final Test Accuracy: {}".format(test_acc))
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+
+    # Select three distinct questions
+    question_ids = [1525, 773, 1103]
+
+    # Plot the probability as a function of θ for the selected questions
+    plot_prob_vs_theta(theta, beta, question_ids)
 
 
 if __name__ == "__main__":
     main()
-
